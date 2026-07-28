e47422754ab13cab8508fee77fc07ba523b7a781
Michael Stashevski <mst.rocking@gmail.com>
2026-07-28 20:32:16 +0300

    chore: remove stray tracked files

--- test-slack-debug.mjs
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import { loadEnv } from './src/config/env.js';
-const { env } = loadEnv();
-console.log('SLACK_BOT_TOKEN:', env.SLACK_BOT_TOKEN ? '***present***' : 'missing');
-console.log('SLACK_CHANNEL:', env.SLACK_CHANNEL || 'missing');
-process.exit(0);
